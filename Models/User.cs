6b121860896ce0199086fcf6cb73edb5029d6555
Cody Mumford <cody.mumford9106@hotmail.com>
2019-07-29 10:38:59 -0400

    modify User.cs and add photo.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace RednitDating.Api.Models
 {
     public class User {
@@ -8,5 +11,26 @@ namespace RednitDating.Api.Models
 
         public byte[] PasswordSalt { get; set; }
 
+        public string Gender { get; set; }
+
+        public string KnownAS { get; set; }
+
+        public DateTime DOB { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime LastActive { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
     }
 }
