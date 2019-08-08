051327540248ae22008c774e2b821648a26fff28
Cody Mumford <cody.mumford9106@hotmail.com>
2019-08-08 15:04:49 -0500

    add filtering by minAge and maxAge

--- Helpers/UserParams.cs
+++ Helpers/UserParams.cs
@@ -12,6 +12,9 @@ namespace RednitDating.Api.Helpers
 
         public int UserId { get; set; }
         public string Gender { get; set; }
+        public int MinAge { get; set; } = 18;
+
+        public int MaxAge { get; set; } = 99;
     }
         
 }
